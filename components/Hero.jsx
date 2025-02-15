bc9386b9de9ed530f521ec44be247334fe851369
ScrewedSoul7 <kaushik.salsingikar@gmail.com>
2025-02-15 15:09:14 -0500

    Initial commit

---
+++ components/Hero.jsx
@@ -0,0 +1,18 @@
+
+export default function Hero ({title,subtitle}) {
+  return (
+    <section className="bg-gradient-to-b from-orange-300 to-pink-500 py-10 mb-4">
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center">
+      <div className="text-center">
+        <h1 className="text-4xl font-extrabold text-white sm:text-5xl md:text-6xl">
+          {title}
+        </h1>
+        <p className="my-4 text-xl text-white">
+          {subtitle}
+        </p>
+      </div>
+    </div>
+  </section>
+  )
+}
+
---
+++ components/Navbar.jsx
@@ -0,0 +1,33 @@
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <nav className="bg-cyan-400 border-b border-cyan-500 text-[#1E3A5F]">
+      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+        <div className="flex h-20 items-center justify-between">
+          <div className="flex flex-1 items-center justify-center md:items-stretch md:justify-start">
+            <Link href="/" className="flex flex-shrink-0 items-center mr-4">
+            <img src="/icon.png" alt="Site Icon" className="h-10 w-auto" />
+              <span className="hidden md:block text-2xl font-bold ml-2">
+                Budget Travel
+              </span>
+            </Link>
+          </div>
+          <div className="md:ml-auto">
+            <div className="flex">
+              <Link href="/" className="font-bold hover:bg-[#008B9E] px-4 h-20 flex items-center justify-center transition">
+                Home
+              </Link>
+              <Link href="/about" className="font-bold hover:bg-[#008B9E] px-4 h-20 flex items-center justify-center transition">
+                About
+              </Link>
+              <Link href="/destinations" className="font-bold hover:bg-[#008B9E] px-4 h-20 flex items-center justify-center transition">
+                Popular Destinations
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>    
+  );
+}
